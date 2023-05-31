e97cabbeb21ab60ee07e622d0fa9e7cf5e0ad7d4
MarcosPrime <135130636+MarcosPrime@users.noreply.github.com>
2023-05-31 10:05:59 -0300

    Sistema de controle de estoque de bicicletaria
    Enunciado: Imagina-se que você está desenvolvendo um software de controle de estoque para uma bicicletaria. Este software deve ter o seguinte menu e opções:
    1.	Cadastrar Peça
    2.	Consultar Peça
    1)	Consultar Todas as Peças
    2)	Consulta Peças por Código
    3)	Consulta Peças por Fabricante
    4)	Retornar 
    3.	Remover Peça
    4.	Sair
    Elabore um programa em Python que:
    1.	Deve-se codificar uma função cadastrarPeca (código) (EXIGÊNCIA 1);
    o	Essa função recebe como parâmetro um código exclusivo para cada peça cadastrado (DICA: utilize um contador como parâmetro) 
    o	Dentro da função perguntar o nome da peça;
    o	Dentro da função perguntar o fabricante da peça;
    o	Dentro da função perguntar o valor da peça
    o	Cada peça cadastrada deve ter os seus dados armazenados num DICIONÁRIO (DICA: Conferir material escrito da p. 22 até p24  da AULA 06)
    2.	Deve-se codificar uma função consultarPeca(EXIGÊNCIA 2);
    o	Dentro da função ter um menu com as seguintes opções:
    	Consultar Todas as Peças
    	Consultar Peças por Código
    	Consultar Peças por Fabricante
    	Retornar
    3.	Deve-se codificar uma função chamada removerPeca (EXIGÊNCIA 3);
    o	Dentro da função perguntar qual o código do produto que se deseja remover do cadastro (da lista de dicionário)
    4.	Colocar um exemplo de SAIDA DO CONSOLE com o cadastro de 3 (ou mais) peças. Sendo que 2 delas do mesmo fabricante – ver figura 1
    5.	Colocar um exemplo de SAIDA DO CONSOLE com a consulta a todas as peças cadastradas – ver figura 2
    6.	Colocar um exemplo de SAIDA DO CONSOLE com uma consulta por código – ver figura 3
    7.	Colocar um exemplo de SAIDA DO CONSOLE com uma consulta por fabricante – ver figura 4
    8.	Colocar um exemplo de SAIDA DO CONSOLE ao remover um cadastro e mostrando depois todos os cadastros– ver figura 5

---
+++ Exemplo4.py
@@ -0,0 +1,4 @@
+"""print(f"|Código      >> {codigo}\n"
+                f"|Peça        >>> {nome_peca} \n"
+                f"|Fabricante  >>> {fabricante_peca}\n"
+                f"|Valor       >>> R$ {valor_peca}\"""
